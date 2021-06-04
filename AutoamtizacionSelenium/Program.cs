7f83f4deb7ef05a7261596d0fecb4e791a098d17
FabioVasquez69 <fabio.vaz93@hotmail.com>
2021-06-04 08:36:52 -0500

    AutomatizacionSelenium

---
+++ AutoamtizacionSelenium/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium;
+
+namespace AutoamtizacionSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new OperaDriver();
+            driver.Url = "https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=azure-devops&tabs=preview-page";
+            driver.Manage().Window.FullScreen();
+
+
+            //driver.Manage().Window.Minimize();
+        }
+    }
+}
